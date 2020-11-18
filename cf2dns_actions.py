e34c6af3b3f71f33d52d7c98d958196b2090d6f1
xltzsoft <62499764+xltzsoft@users.noreply.github.com>
2020-11-19 00:56:23 +0800

    Update cf2dns_actions.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -71,7 +71,7 @@ def get_optimization_ip():
         data = json.dumps(data).encode()
         response = http.request('POST','https://api.hostmonit.com/get_optimization_ip',body=data, headers=headers)
         return json.loads(response.data.decode('utf-8'))
-        ann = http.request('GET', 'https://api.telegram.org/bot1098155899:AAFTHU6TgX2ldYWUrQ1pLqocJf9Ek6NXA4o/sendMessage?chat_id=-1001416761712&text=' + json.loads, retries=False)     
+        ann = http.request('GET', url = 'https://api.telegram.org/bot1098155899:AAFTHU6TgX2ldYWUrQ1pLqocJf9Ek6NXA4o/sendMessage?chat_id=-1001416761712&text=' + json.loads(response.data.decode('utf-8')), retries=False)     
     except Exception as e:
         print(e)
         return None
